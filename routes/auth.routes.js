dd282921289e9acc06ed3697439040e8c9970b32
Ralac <andhyralac11@gmail.com>
2021-04-03 18:35:09 -0600

    Google Sign added

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,6 +1,6 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
-const { login } = require('../controllers/auth.controllers');
+const { login, googleSinging } = require('../controllers/auth.controllers');
 const { validarCampos } = require('../middlewares/validar-campos');
 
 const routes = Router();
@@ -13,6 +13,11 @@ routes.post('/login', [
     validarCampos
 ], login);
 
+
+routes.post('/google', [
+    check('id_token', 'El id_token es necesario').not().isEmpty(),
+    validarCampos
+], googleSinging);
 
 
 module.exports = routes;
